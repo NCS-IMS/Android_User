e102839bd64ea5c8f2fb3a82397c7ce9338a9382
Deuk_Hoi <dh97k@naver.com>
2021-05-12 13:47:31 +0900

    complete find Road list function

--- app/src/main/java/com/ncs/imsUser/ItemAdapterManager/LocationListAdapter.kt
+++ app/src/main/java/com/ncs/imsUser/ItemAdapterManager/LocationListAdapter.kt
@@ -1,21 +1,14 @@
 package com.ncs.imsUser.ItemAdapterManager
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import android.widget.TextView
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.ncs.imsUser.HTTPManager.DTOManager.FindData
-import com.ncs.imsUser.R
 import com.ncs.imsUser.databinding.ItemLocationBinding
+import java.util.*
 
-class LocationListAdapter(var context: Context, var findLoadList : List<FindData>, var itemClick: (FindData)->Unit):RecyclerView.Adapter<LocationListAdapter.LocationListViewHolder>(){
+class LocationListAdapter(var context: Context, var findLoadList: List<FindData>, var itemClick: (FindData) -> Unit):RecyclerView.Adapter<LocationListAdapter.LocationListViewHolder>(){
     inner class LocationListViewHolder(itemLocationBinding: ItemLocationBinding, itemClick: (FindData) -> Unit?) : RecyclerView.ViewHolder(itemLocationBinding.root){
         var title = itemLocationBinding.locationTitle
         var address = itemLocationBinding.addressTxt
@@ -26,25 +19,32 @@ class LocationListAdapter(var context: Context, var findLoadList : List<FindData
             title.text = findData.place_name
             address.text = findData.address_name
             phone.text = findData.phone
-            direction.text = findData.distance
-
+            direction.text = unitConveter(findData.distance)
             itemView.setOnClickListener{itemClick(findData)}
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup,viewType: Int): LocationListAdapter.LocationListViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LocationListAdapter.LocationListViewHolder {
         var itemLocationBinding = ItemLocationBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        Log.e("dfs", findLoadList.toString())
-        Log.e("1234", "sdfsdfs")
         return LocationListViewHolder(itemLocationBinding, itemClick)
     }
 
-    override fun onBindViewHolder(holder: LocationListAdapter.LocationListViewHolder,position: Int) {
+    override fun onBindViewHolder(holder: LocationListAdapter.LocationListViewHolder, position: Int) {
         holder.bind(findLoadList[position], context)
     }
 
     override fun getItemCount(): Int {
         return findLoadList.size
+    }
+
+    //거리 단위 변환
+    fun unitConveter(distance: String):String{
+        if(distance.length < 4){
+            return distance +"m"
+        }else{
+            val d: Double = java.lang.String.format(Locale.KOREAN, "%.2f", distance.toDouble()/1000).toDouble() //소수점 둘째 자리까지 변환
+            return d.toString()+"Km"
+        }
     }
 
 
